e5609d7420f3dde7be096e8426358c04c8314896
cgripaldo <cgripaldo@gmail.com>
2020-03-08 13:38:29 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# flask_task_manager
-From https://github.com/jakerieger/FlaskIntroduction
+# Task List Web Application
+## A simple Flask web application with CRUD database functionality
+### Reference https://github.com/jakerieger/FlaskIntroduction
+
+---
+## Create the database 
+```bash
+$python
+```
+```python
+>>from app import db
+>>db.create_all() 
+```
+Reference https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/  
+
+---
+## Starting and stopping the web app
+```bash
+$python app.py
+```
+Then open http://127.0.0.1:5000/ in your web browser.
+Press CTRL+C to quit.
+
+---
+## Changes to the original reference code
+Add "required" attribute to the HTML create and update POST forms
+```html
+<div class="form">
+    <form action="/" method="POST">
+        <input type="text" name="content" id="content" required>
+        <input type="submit" value="Add Task">
+    </form>
+</div>
+```
+Reference: https://stackoverflow.com/questions/9844150/flask-sqlalchemy-nullable-false/9845764  
+  
+---
+## Fix on VS Code linting errors on flask_sqlalchemy SQLAlchemy member functions
+Add to VS Code settings
+```
+"python.linting.pylintArgs": ["--load-plugins", "pylint_flask"]
+```
+Reference https://stackoverflow.com/questions/53975234/instance-of-sqlalchemy-has-no-column-member  
---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+# add to VS Code settings "python.linting.pylintArgs": ["--load-plugins", "pylint_flask"] to remove linting errors on flask sqlalchemy member functions #reference https://stackoverflow.com/questions/53975234/instance-of-sqlalchemy-has-no-column-member
+# create database: python; from app import db; db.create_all() #reference https://flask-sqlalchemy.palletsprojects.com/en/2.x/quickstart/
+# add "required" attribute to the html create and update POST forms # reference: https://stackoverflow.com/questions/9844150/flask-sqlalchemy-nullable-false/9845764
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
